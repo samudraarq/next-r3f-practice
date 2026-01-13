50795fddc9bd7f1ac169d82fe4a7e133e569c522
Samudra Faris <samudrafaris@gmail.com>
2026-01-13 13:55:57 +0700

    install r3f and basic canvas setup

--- app/playgrounds/particle-cursor-animation/page.tsx
+++ app/playgrounds/particle-cursor-animation/page.tsx
@@ -1,4 +1,27 @@
+"use client";
+
+import { Box, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
 const Page = () => {
-  return <div>Particle Cursor Animation</div>;
+  return (
+    <div id="canvas-container" className="canvas-fullscreen">
+      <Canvas>
+        <OrbitControls />
+
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </div>
+  );
 };
 export default Page;
