091a6ebfa232584adca4cd6dc43dd0cca2f9758c
Samudra Faris <samudrafaris@gmail.com>
2026-01-15 16:47:20 +0700

    draw a 2d canvas

--- app/playgrounds/particle-cursor-animation/experience.tsx
+++ app/playgrounds/particle-cursor-animation/experience.tsx
@@ -1,9 +1,10 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { extend, useFrame } from "@react-three/fiber";
 import { OrbitControls, shaderMaterial, useTexture } from "@react-three/drei";
 import * as THREE from "three";
 import fragmentShader from "./shaders/fragment.glsl";
 import vertextShader from "./shaders/vertex.glsl";
+import glowImage from "@/public/particle-cursor-animation/glow.png";
 
 const ParticlesMaterial = shaderMaterial(
   {
@@ -21,9 +22,27 @@ type ParticlesMaterialType = THREE.ShaderMaterial & {
 
 const PartMaterial = extend(ParticlesMaterial);
 
-const Experience = () => {
+type Props = {
+  canvasRef: React.RefObject<HTMLCanvasElement>;
+};
+
+const Experience = ({ canvasRef }: Props) => {
   const particlesMaterialRef = useRef<ParticlesMaterialType>(null!);
   const texture = useTexture("/particle-cursor-animation/picture-1.png");
+  const interactivePlaneRef = useRef<THREE.Mesh>(null!);
+  const canvasContextRef = useRef<CanvasRenderingContext2D | null>(null);
+
+  const image = new Image();
+  image.src = glowImage.src;
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d")!;
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    canvasContextRef.current = context;
+  }, [canvasRef]);
+
+  const raycaster = new THREE.Raycaster();
 
   useFrame((state) => {
     particlesMaterialRef.current.uResolution = new THREE.Vector2(
@@ -31,6 +50,31 @@ const Experience = () => {
       state.size.height * state.viewport.dpr,
     );
     particlesMaterialRef.current.uPictureTexture = texture;
+
+    // Raycaster update
+    raycaster.setFromCamera(state.pointer, state.camera);
+    const intersections = raycaster.intersectObject(
+      interactivePlaneRef.current,
+    );
+    if (intersections.length) {
+      const uv = intersections[0].uv;
+
+      if (uv && canvasContextRef.current) {
+        const x = uv.x * canvasRef.current.width;
+        const y = (1 - uv.y) * canvasRef.current.height;
+        const glowSize = canvasRef.current.width * 0.25;
+
+        // Draw on canvas
+        canvasContextRef.current.globalCompositeOperation = "lighten";
+        canvasContextRef.current.drawImage(
+          image,
+          x - glowSize / 2,
+          y - glowSize / 2,
+          glowSize,
+          glowSize,
+        );
+      }
+    }
   });
 
   return (
@@ -46,6 +90,12 @@ const Experience = () => {
           ref={particlesMaterialRef}
         />
       </points>
+
+      {/* Interactive Plane */}
+      <mesh ref={interactivePlaneRef}>
+        <planeGeometry args={[10, 10]} />
+        <meshBasicMaterial color="red" />
+      </mesh>
     </>
   );
 };
--- app/playgrounds/particle-cursor-animation/page.tsx
+++ app/playgrounds/particle-cursor-animation/page.tsx
@@ -2,15 +2,25 @@
 
 import { Canvas } from "@react-three/fiber";
 import dynamic from "next/dynamic";
+import { useRef } from "react";
 
 const Experience = dynamic(() => import("./experience"), { ssr: false });
 
 const Page = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null!);
+
   return (
     <div id="canvas-container" className="canvas-fullscreen">
       <Canvas camera={{ position: [0, 0, 18], fov: 35 }}>
-        <Experience />
+        <Experience canvasRef={canvasRef} />
       </Canvas>
+
+      <canvas
+        ref={canvasRef}
+        width={128}
+        height={128}
+        className="fixed top-0 left-0 z-10 w-64 h-64"
+      />
     </div>
   );
 };
